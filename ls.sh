c6a21e41ac0e578a188a956b87f372ee07fcefc5
Luke Marsden <luke@clusterhq.com>
2015-01-06 18:21:47 +0000

    ls on two OSs

--- ls.sh
+++ ls.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# Hack to make colour ls work on Linux, FreeBSD *and* OSX
-gnuls --color=auto $* 2>/dev/null || /bin/ls --color=auto $* 2>/dev/null || ls $*
+# Hack to make colour ls work on Linux *and* OSX
+/bin/ls --color=auto $* 2>/dev/null || ls $*
 
