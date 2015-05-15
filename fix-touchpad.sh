0268bccc648ec63cbec926d1a1afa56cad3f2337
Luke Marsden <luke@clusterhq.com>
2015-05-15 13:57:10 +0100

    tweak

--- fix-touchpad.sh
+++ fix-touchpad.sh
@@ -8,4 +8,4 @@ sudo iwconfig wlan0 power off
 gsettings set org.gnome.settings-daemon.plugins.cursor active false
 gsettings set org.gnome.desktop.a11y.keyboard bouncekeys-enable true
 gsettings set org.gnome.desktop.a11y.keyboard bouncekeys-delay 50
-syndaemon -i 0.3 -k -R
+syndaemon -i 0.2 -k -R
