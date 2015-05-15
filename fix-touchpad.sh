e4dc9798ffa54a2139084ad456beac620ad7d38a
Luke Marsden <luke@clusterhq.com>
2015-05-15 13:51:54 +0100

    tweak settings

--- fix-touchpad.sh
+++ fix-touchpad.sh
@@ -4,5 +4,7 @@ synclient RTCornerButton=0
 synclient RBCornerButton=0
 synclient LTCornerButton=0
 synclient LBCornerButton=0
-gsettings set org.gnome.settings-daemon.plugins.cursor active false
 sudo iwconfig wlan0 power off
+gsettings set org.gnome.settings-daemon.plugins.cursor active false
+gsettings set org.gnome.desktop.a11y.keyboard bouncekeys-enable true
+gsettings set org.gnome.desktop.a11y.keyboard bouncekeys-delay 50
